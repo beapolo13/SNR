bc83f0a5c75dd81beff909cbae255dfad1ea44d1
beapolo13 <beapolo2000@gmail.com>
2024-03-18 15:25:59 +0100

    .

--- test.py
+++ test.py
@@ -4,11 +4,4 @@ from covariance_matrix import *
 from expectation_values import *
 
 
-ratio_plots(2,[-1])
-ratio_plots(2,[-1,-1])
-ratio_plots(2,[-1,-1,-1])   
-
-ratio_plots(2,[1])
-ratio_plots(2,[1,1])
-ratio_plots(2,[1,1,1]) 
-    
+ratio_plots(2)
