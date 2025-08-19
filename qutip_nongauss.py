facac62a1634e84feb70c45afb04bb4d1f92f032
Beatriz Polo Rodriguez <bpolo@icfo.net>
2025-08-19 23:17:33 +0200

    .

--- qutip_nongauss.py
+++ qutip_nongauss.py
@@ -9,6 +9,7 @@ from scipy.optimize import minimize
 from scipy.linalg import expm
 from scipy.ndimage import gaussian_filter
 from scipy.interpolate import RectBivariateSpline
+from qutip.continuous_variables import covariance_matrix
 
 
 def r(z):
@@ -72,6 +73,35 @@ def photon_subtract(rho, cutoff, mode=[1,0]): #the mode vector indicates the wei
     rho_sub = rho_sub / rho_sub.tr()  # Normalize
 
     return rho_sub
+
+def covariance_matrix_check(t,z,x,cutoff,phi):
+    #this function checks if the covariance matrix of the above-created photon subtracted state is the same as that in the paper by Barral et al
+    
+    modes=2
+    rho=photon_subtract(create_two_mode_gaussian_state(t, t, z, 1/z, x, cutoff),cutoff,[np.cos(phi),np.sin(phi)])
+    a_ops = [ qt.tensor([qt.destroy(rho.dims[0][j]) if j == m else qt.qeye(rho.dims[0][j])
+                   for j in range(modes)]) for m in range(modes)]
+    
+    # quadratures
+    q_ops = [(a + a.dag())/np.sqrt(2) for a in a_ops]
+    p_ops = [(a - a.dag())/(1j*np.sqrt(2)) for a in a_ops]
+    basis_ops = q_ops + p_ops
+    sigma_0 = np.array(State(2,[z,1/z],[x],[0,0],temp=[t,t]).matrix)
+    identity4= qt.qeye(4).full()
+    def P(phi):
+        return np.array([
+            [np.cos(phi)**2,   0.5*np.sin(2*phi),0, 0],
+            [0.5*np.sin(2*phi),np.sin(phi)**2,0,0],
+            [ 0,  0,np.cos(phi)**2,   0.5*np.sin(2*phi)],
+            [0, 0, 0.5*np.sin(2*phi),   np.sin(phi)**2]])
+    P = P(phi)
+    print('test1',sigma_0,identity4,P,np.matmul(sigma_0-identity4,P) )
+    print(np.trace((sigma_0-identity4)@P))
+    cov1=covariance_matrix(basis_ops,rho)
+    cov2=(sigma_0 + 2*(np.matmul((sigma_0-identity4)@P,(sigma_0-identity4)))/np.trace((sigma_0-identity4)@P))/2
+    print('Numerical matrix',cov1)
+    print('Exact matrix', np.round(cov2,3))
+    return cov1 - cov2
 
 def log_negativity(rho):
     return negativity(rho, 1, method='tracenorm', logarithmic=True)
@@ -244,22 +274,69 @@ def local_ergotropy_qutip(rho, dA, dB, omegaA=1.0, omegaB=1.0):
     return ergA + ergB
 
 def relative_ergotropic_gap_qutip(rho,H,dA,dB):
-    result= (ergotropy_qutip(rho, H) - local_ergotropy_qutip(rho,dA,dB))/ergotropy_qutip(rho,H)
+    ge=ergotropy_qutip(rho, H)
+    le=local_ergotropy_qutip(rho,dA,dB)
+    result= (ge - le)/ge
     print('REG calculated')
     return result
 
+def gaussian_reg_phsub_on_superposition_ofmodes(z1,z2,phi,nu):
+  def tests_ph_subnongauss(z1,z2,phi,nu):
+    V_0 = np.array([[nu*z1,0,0,0],[0,nu/z1,0,0],[0,0,nu*z2,0],[0,0,0,nu/z2]])
+    def P(phi):
+        return np.array([
+            [np.cos(phi)**2, 0,  0.5*np.sin(2*phi),0],
+            [0, np.cos(phi)**2, 0,  0.5*np.sin(2*phi)],
+            [0.5*np.sin(2*phi),0, np.sin(phi)**2,0],
+            [0, 0.5*np.sin(2*phi), 0,  np.sin(phi)**2]])
+    P = P(phi)
+    Id = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    result_prel = (V_0 - Id)@P
+    trace1 = result_prel.trace()
+    result = V_0 + (2/trace1)*(result_prel@(V_0-Id))
+    return result
+
+  def apply_local_sq(matrix, s1,s2):
+    S = np.array([[s1,0,0,0],[0,1/s1,0,0],[0,0,s2,0],[0,0,0,1/s2]])
+    return S @ matrix @S.T
+  res= tests_ph_subnongauss(z1,z2,phi,nu)
+  #print(res)
+  #print(res[1][1], res[3][3])
+
+  std= apply_local_sq(res,np.sqrt(np.sqrt(res[1][1]/res[0][0])),np.sqrt(np.sqrt(res[3][3]/res[2][2])))
+  #print(std)
+  a= std[0][0]
+  b= std[2][2]
+  c1 = std[0][2]
+  c2= std[1][3]
+
+  #print(a,b,c1,c2)
+
+  gamma = a**2 + b**2 + 2*c1*c2
+  #print('gamma', gamma)
+  det = np.linalg.det(res)
+  #print(det)
+  nu_plus= np.sqrt((gamma + np.sqrt(gamma**2 -4*det))/2)
+  nu_minus = np.sqrt((gamma - np.sqrt(gamma**2 -4*det))/2)
+  #print('nu_plus', nu_plus)
+  #print('nu_min', nu_minus)
+
+  EG = 0.5*(a+b-nu_plus-nu_minus)
+  eg= 0.5*(nu_plus+nu_minus)-1
+  return EG/eg
+
 
 def plot_general_ergotropy(type_of_state):
-    epsilon = 1e-3
+    epsilon = 1e-4
     cutoff=10
     hamiltonian=two_mode_ho_hamiltonian_qutip(cutoff,cutoff)
-    x_points=20
-    y_points=20
+    x_points=100
+    y_points=100
 
     if type_of_state == 'tms photonsub on mode 1':
         z_vec=np.linspace(0.5,1,x_points)
         r_vec= np.array([-np.log(z)/2 for z in z_vec])
-        t_vec = np.linspace(0.5,6,y_points)
+        t_vec = np.linspace(0.1,6,y_points)
         k_vec= np.array([1/np.tanh((1/(2*t))) for t in t_vec])
         x= np.pi/4
         X=z_vec
@@ -269,25 +346,61 @@ def plot_general_ergotropy(type_of_state):
         en = [[np.float64(log_negativity(photon_subtract(create_two_mode_gaussian_state(t_vec[j], t_vec[j], z,1/z, x, cutoff),cutoff,[1,0]))) for z in z_vec] for j in range(len(k_vec))]
 
     if type_of_state == 'tms photonsub on superposition of modes':
-        z=0.5
-        phi_vec=np.linspace(0,np.pi/4,x_points)
-        t_vec = np.linspace(0.5,6,y_points)
+        z1=0.5
+        z2=0.5
+        phi_vec=np.linspace(0,np.pi/2,x_points)
+        t_vec = np.linspace(0.1,6,y_points)
         k_vec= np.array([1/np.tanh((1/(2*t))) for t in t_vec])
-        x= np.pi/4
+        x= 0
         X=phi_vec
         Y=k_vec
-        #W = [[np.float64(find_ergotropic_gap_phsub(z,k)) for z in z_vec] for k in k_vec]
-        W = [[np.float64(relative_ergotropic_gap_qutip(photon_subtract(create_two_mode_gaussian_state(t_vec[j], t_vec[j], z, 1/z, x, cutoff),cutoff,[np.cos(phi),np.sin(phi)]),hamiltonian,cutoff,cutoff)) for phi in phi_vec] for j in range(len(k_vec))]
-        en = [[np.float64(log_negativity(photon_subtract(create_two_mode_gaussian_state(t_vec[j], t_vec[j], z,1/z, x, cutoff),cutoff,[np.cos(phi),np.sin(phi)]))) for phi in phi_vec] for j in range(len(k_vec))]
+        #W = [[np.float64(gaussian_reg_phsub_on_superposition_ofmodes(z1,z2,phi,nu)) for phi in phi_vec] for nu in k_vec]
+        #W = [[np.float64(relative_ergotropic_gap_qutip(photon_subtract(create_two_mode_gaussian_state(t_vec[j], t_vec[j], z1, z2, x, cutoff),cutoff,[np.cos(phi),np.sin(phi)]),hamiltonian,cutoff,cutoff)) for phi in phi_vec] for j in range(len(k_vec))]
+        en = [[np.float64(log_negativity(photon_subtract(create_two_mode_gaussian_state(t_vec[j], t_vec[j], z1,z2, x, cutoff),cutoff,[np.cos(phi),np.sin(phi)]))) for phi in phi_vec] for j in range(len(k_vec))]
+
+    if type_of_state == 'noon':
+        def create_mixed_noon(N,t,p):
+            noon = (qt.tensor(qt.basis(cutoff, N), qt.basis(cutoff, 0)) + qt.tensor(qt.basis(cutoff, 0), qt.basis(cutoff, N))).unit()
+            rho_noon = noon.proj()
+            thermal1 = qt.thermal_dm(cutoff, n_th(t))
+            thermal2 = qt.thermal_dm(cutoff, n_th(t))
+            rho_thermal = qt.tensor(thermal1, thermal2)
+            return p * rho_noon + (1 - p) * rho_thermal
+        
+        p_vec=np.linspace(0.1,1,x_points)   #mixedness
+        t_vec = np.linspace(0.1,3,y_points)   
+        k_vec= np.array([1/np.tanh((1/(2*t))) for t in t_vec])
+        X=p_vec
+        Y=k_vec
+        W = [[np.float64(relative_ergotropic_gap_qutip(create_mixed_noon(5,t_vec[j],p),hamiltonian,cutoff,cutoff)) for p in p_vec] for j in range(len(k_vec))]
+        en = [[np.float64(log_negativity(create_mixed_noon(3,t_vec[j],p))) for p in p_vec] for j in range(len(k_vec))]
+
+    if type_of_state == 'cat':
+        def create_2mode_mixed_cat(alpha,p):
+            cs_pp = qt.tensor(qt.coherent(cutoff, alpha), qt.coherent(cutoff, alpha))
+            cs_mm = qt.tensor(qt.coherent(cutoff, -alpha), qt.coherent(cutoff, -alpha))
+            rho_cat2 = (cs_pp + cs_mm).unit().proj()
+            rho_cat2_odd = (cs_pp - cs_mm).unit().proj()
+            rho_th = qt.tensor(qt.thermal_dm(cutoff, n_th(t)), qt.thermal_dm(cutoff, n_th(t)))
+            return p * rho_cat2 + (1 - p) * rho_cat2_odd
+        
+        p_vec=np.linspace(0.1,1,x_points)   #mixedness
+        alpha_vec = np.linspace(0.1,6,y_points)   
+        k_vec= np.array([1/np.tanh((1/(2*t))) for t in t_vec])
+        X=alpha_vec
+        Y=p_vec
+        W = [[np.float64(relative_ergotropic_gap_qutip(create_2mode_mixed_cat(alpha_vec[j],p),hamiltonian,cutoff,cutoff))  for j in range(len(alpha_vec))] for p in p_vec]
+        en = [[np.float64(log_negativity(create_2mode_mixed_cat(alpha_vec[j],p)))  for j in range(len(alpha_vec))] for p in p_vec]
+
 
     X_grid, Y_grid =np.meshgrid(X,Y)
     fig,ax=plt.subplots(1,1,figsize=(10,6))
-    c1=ax.pcolormesh(X_grid,Y_grid,W,cmap=cm.get_cmap('viridis_r', 15))
+    c1=ax.pcolormesh(X_grid,Y_grid,en,cmap=cm.get_cmap('viridis_r', 20))
     #c2=ax[1].pcolormesh(X_grid,Y_grid,en,cmap=cm.get_cmap('viridis_r', 50))
     #c2=ax[1].pcolormesh(X_grid,Y_grid,sep,cmap=cm.get_cmap('viridis', 10))
     cbar1=fig.colorbar(c1,ax=ax, label=r'$\Delta \epsilon_{\text{rel}}$')
     #cbar2=fig.colorbar(c2,ax=ax[1], label=f'logarithmic negtivity')
-    contour = ax.contour(X_grid, Y_grid,en,levels=[epsilon], colors='black', linestyles='dashed', linewidths=1.5)
+    #contour = ax.contour(X_grid, Y_grid,en,levels=[epsilon], colors='black', linestyles='dashed', linewidths=1.5)
 
     
     #ax.clabel(contour, inline=True, fontsize=30,fmt='log neg')
@@ -306,11 +419,28 @@ def plot_general_ergotropy(type_of_state):
     ax.set_yticks(y)
     #ax[1].set_yticks(y)
     #beep()
-    #plt.savefig()
+    plt.savefig('Photon subtraction on superposition of modes_ LOG NEG')
     beep()
     plt.show()
     return
 
+def tests_ph_subnongauss(z1,z2,phi,nu):
+    V_0 = np.array([[nu*z1,0,0,0],[0,nu/z1,0,0],[0,0,nu*z2,0],[0,0,0,nu/z2]])
+    def P(phi):
+        return np.array([
+            [np.cos(phi)**2, 0,  0.5*np.sin(2*phi),0],
+            [0, np.cos(phi)**2, 0,  0.5*np.sin(2*phi)],
+            [0.5*np.sin(2*phi),0, np.sin(phi)**2,0],
+            [0, 0.5*np.sin(2*phi), 0,  np.sin(phi)**2]])
+    P = P(phi)
+    Id = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
+    result_prel = (V_0 - Id)@P
+    trace1 = result_prel.tr()
+    result = V_0 + (2/trace1)*(result_prel@(V_0-Id))
+    return result
+
+
+#covariance_matrix_check(2,0.6,np.pi/4,35,np.pi/6)
 plot_general_ergotropy('tms photonsub on superposition of modes')
 #PARAMETERS
 t1= 1.5  #temperature of first mode (between 0 and infty)
